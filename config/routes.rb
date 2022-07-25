a8e965a5db5443d1b19f5d37cde7352303eb1361
dzdn <jdustinwilliams@gmail.com>
2022-07-24 21:20:29 -0400

    Add User model and functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :edit, :update]
 
   # Defines the root path route ("/")
   # root "articles#index"
